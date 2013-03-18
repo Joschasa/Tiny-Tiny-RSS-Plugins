f9d2b57721cf4623fec6478b12e59146ab2606b6
Joschasa <joschasa@lavabit.com>
2013-03-18 21:58:37 +0100

    Add af_nichtlustig

---
+++ af_nichtlustig/init.php
@@ -0,0 +1,56 @@
+<?php
+class Af_NichtLustig extends Plugin {
+
+	private $link;
+	private $host;
+
+	function about() {
+		return array(1.0,
+			"Remove unnecessary stuff from Nicht Lustig feed",
+			"Joschasa");
+	}
+
+	function init($host) {
+		$this->link = $host->get_link();
+		$this->host = $host;
+
+		$host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
+	}
+
+	function hook_article_filter($article) {
+		$owner_uid = $article["owner_uid"];
+
+		$force = false;
+
+		if (strpos($article["link"], "nichtlustig.de") !== FALSE) {
+			if (strpos($article["plugin_data"], "nichtlustig,$owner_uid:") === FALSE || $force) {
+				$doc = new DOMDocument();
+				@$doc->loadHTML($article["content"]);
+
+				if ($doc) {
+					$xpath = new DOMXPath($doc);
+					$entries = $xpath->query('(//img[@src])');
+
+					$found = false;
+
+					foreach ($entries as $entry) {
+						if (preg_match("/(http:\/\/.*\/comics\/full\/.*)/i", $entry->getAttribute("src"))) {
+							$found = $entry;
+							break;
+						}
+					}
+
+					if ($found) {
+						$article["content"] = $doc->saveXML($found);
+						$article["plugin_data"] = "nichtlustig,$owner_uid:" . $article["plugin_data"];
+					}
+				}
+			} else if (isset($article["stored"]["content"])) {
+				$article["content"] = $article["stored"]["content"];
+			}
+		}
+
+		return $article;
+	}
+}
+?>
