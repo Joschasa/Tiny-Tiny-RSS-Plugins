c5f511c268a4d9f2821fb0ff40dfaa62893a9e02
Joschasa <joschasa@lavabit.com>
2013-03-19 00:20:48 +0100

    Add af_nerfnow

---
+++ af_nerfnow/init.php
@@ -0,0 +1,59 @@
+<?php
+class Af_NerfNow extends Plugin {
+
+	private $link;
+	private $host;
+
+	function about() {
+		return array(1.0,
+			"Bigger inline image for NerfNow",
+			"Joschasa");
+	}
+
+	function init($host) {
+		$this->link = $host->get_link();
+		$this->host = $host;
+
+		$host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
+	}
+
+	function hook_article_filter($article) {
+		$owner_uid = $article["owner_uid"];
+
+		$force = false;
+
+		if (strpos($article["guid"], "nerfnow.com") !== FALSE) {
+			if (strpos($article["plugin_data"], "nerfnow,$owner_uid:") === FALSE || $force) {
+				$doc = new DOMDocument();
+				@$doc->loadHTML($article["content"]);
+
+				if ($doc) {
+					$xpath = new DOMXPath($doc);
+					$entries = $xpath->query('(//img[@src])');
+
+					$found = false;
+
+					foreach ($entries as $entry) {
+						$src = $entry->getAttribute("src");
+						$src = preg_replace("/\/thumb\//", "/image/", $src);
+						$src = preg_replace("/\/large/", "", $src);
+						$entry->setAttribute("src", $src);
+						$found = true;
+					}
+
+					$node = $doc->getElementsByTagName('body')->item(0);
+
+					if ($node && $found) {
+						$article["content"] = $doc->saveXML($node);
+						/* $article["plugin_data"] = "nerfnow,$owner_uid:" . $article["plugin_data"]; */
+					}
+				}
+			} else if (isset($article["stored"]["content"])) {
+				$article["content"] = $article["stored"]["content"];
+			}
+		}
+
+		return $article;
+	}
+}
+?>
