dd2d6d8361341e6ac9945612ee5bf734e603be18
Joschasa <joschasa@lavabit.com>
2013-03-18 23:47:10 +0100

    Add af_golem

---
+++ af_golem/init.php
@@ -0,0 +1,63 @@
+<?php
+class Af_Golem extends Plugin {
+
+	private $link;
+	private $host;
+
+	function about() {
+		return array(1.0,
+			"Load complete golem article into feed.",
+			"Joschasa");
+	}
+
+	function init($host) {
+		$this->link = $host->get_link();
+		$this->host = $host;
+
+		$host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
+	}
+
+	function hook_article_filter($article) {
+		$owner_uid = $article["owner_uid"];
+
+		if (strpos($article["link"], "golem.de") !== FALSE) {
+			if (strpos($article["plugin_data"], "golem,$owner_uid:") === FALSE) {
+
+				$doc = new DOMDocument();
+				@$doc->loadHTML('<?xml encoding="UTF-8">'.fetch_file_contents($article["link"]));
+
+				$basenode = false;
+
+				if ($doc) {
+					$xpath = new DOMXPath($doc);
+
+					// first remove advertisement stuff
+					$stuff = $xpath->query('(//script)|(//noscript)|(//div[@class="iqadcenter"])');
+
+					foreach ($stuff as $removethis) {
+						$removethis->parentNode->removeChild($removethis);
+					}
+
+					// now get the (cleaned) article
+					$entries = $xpath->query('(//article)');
+
+					foreach ($entries as $entry) {
+
+						$basenode = $entry;
+						break;
+					}
+
+					if ($basenode) {
+						$article["content"] = $doc->saveXML($basenode);
+						$article["plugin_data"] = "golem,$owner_uid:" . $article["plugin_data"];
+					}
+				}
+			} else if (isset($article["stored"]["content"])) {
+				$article["content"] = $article["stored"]["content"];
+			}
+		}
+
+		return $article;
+	}
+}
+?>
