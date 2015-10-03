a315df727e34ccb1144ab0a662614ccdc20d9513
André Glüpker <git@wgmd.de>
2015-10-03 13:14:59 +0200

    Add comic filter for german comic erzaehlmirnix

---
+++ af_comics/filters/af_comics_erzaehlmirnix.php
@@ -0,0 +1,43 @@
+<?php
+class Af_Comics_Erzaehlmirnix extends Af_ComicFilter {
+
+    function supported() {
+        return array("erzaehlmirnix");
+    }
+
+    function process(&$article) {
+        $owner_uid = $article["owner_uid"];
+
+        if (strpos($article["link"], "erzaehlmirnix.wordpress.com/") !== FALSE) {
+            if (strpos($article["plugin_data"], "af_comics,$owner_uid:") === FALSE) {
+                $doc = new DOMDocument();
+                @$doc->loadHTML(fetch_file_contents($article["link"]));
+
+                $basenode = false;
+
+                if ($doc) {
+                    $xpath = new DOMXPath($doc);
+                    $entries = $xpath->query('(//a/img[contains(@src, "erzaehlmirnix.files.wordpress.com")])');
+
+                    $found = false;
+
+                    foreach ($entries as $entry) {
+                        $basenode = $entry;
+                    }
+
+                    if ($basenode) {
+                        $article["content"] = $doc->saveXML($basenode);
+                        $article["plugin_data"] = "af_comics,$owner_uid:" . $article["plugin_data"];
+                    }
+                }
+            } else if (isset($article["stored"]["content"])) {
+                $article["content"] = $article["stored"]["content"];
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+}
+?>
