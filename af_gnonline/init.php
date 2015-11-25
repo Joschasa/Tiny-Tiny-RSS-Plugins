c756cbaa36522f426e7d31211639ed32a49585a3
André Glüpker <git@wgmd.de>
2015-11-25 11:12:45 +0100

    Add af_gnonline

---
+++ af_gnonline/init.php
@@ -0,0 +1,47 @@
+<?php
+class Af_GNOnline extends Plugin {
+
+    private $host;
+
+    function about() {
+        return array(1.0,
+            "Fetch content of gn-online.de feed",
+            "Joschasa");
+    }
+
+    function api_version() {
+        return 2;
+    }
+
+    function init($host) {
+        $this->host = $host;
+
+        $host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
+    }
+
+    function hook_article_filter($article) {
+        if (strpos($article["link"], "gn-online.de") !== FALSE) {
+            $doc = new DOMDocument();
+            @$doc->loadHTML(mb_convert_encoding(fetch_file_contents($article["link"]), 'HTML-ENTITIES', "UTF-8"));
+
+            $basenode = false;
+
+            if ($doc) {
+                $xpath = new DOMXPath($doc);
+                $entries = $xpath->query('(//div[@class="StoryShowBaseTextBox"])');
+
+                foreach ($entries as $entry) {
+
+                    $basenode = $entry;
+                    break;
+                }
+
+                if ($basenode) {
+                    $article["content"] = $doc->saveXML($basenode);
+                }
+            }
+        }
+        return $article;
+    }
+}
+?>
