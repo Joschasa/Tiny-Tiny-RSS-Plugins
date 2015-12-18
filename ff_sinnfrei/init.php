30a5cf107d265fc8d9336dea2011b7fcc401eda6
André Glüpker <git@wgmd.de>
2015-12-18 14:10:09 +0100

    Little hack for sinn-frei.com feed with HTML-Entities in XML

---
+++ ff_sinnfrei/init.php
@@ -0,0 +1,49 @@
+<?php
+class ff_sinnfrei extends Plugin {
+
+    private $host;
+
+    function about() {
+        return array(1.0,
+            "Fix feed of sinn-frei.com (remove html umlauts in title)",
+            "Joschasa");
+    }
+
+    function api_version() {
+        return 2;
+    }
+
+    function init($host) {
+        $this->host = $host;
+
+        $host->add_hook($host::HOOK_FEED_FETCHED, $this);
+    }
+
+    function hook_feed_fetched($feed_data, $feed_url, $owner, $feed) {
+        if (strpos($feed_url, "sinn-frei.com") !== FALSE) {
+            _debug("New plugin up and running!");
+
+            $suchmuster = array();
+            $suchmuster[0] = '/&uuml;/';
+            $suchmuster[1] = '/&auml;/';
+            $suchmuster[2] = '/&ouml;/';
+            $suchmuster[3] = '/&Uuml;/';
+            $suchmuster[4] = '/&Auml;/';
+            $suchmuster[5] = '/&Ouml;/';
+            $suchmuster[6] = '/&szlig/';
+
+            $ersetzungen = array();
+            $ersetzungen[0] = 'ü';
+            $ersetzungen[1] = 'ä';
+            $ersetzungen[2] = 'ö';
+            $ersetzungen[3] = 'Ü';
+            $ersetzungen[4] = 'Ä';
+            $ersetzungen[5] = 'Ö';
+            $ersetzungen[6] = 'ß';
+
+            $feed_data = preg_replace($suchmuster, $ersetzungen, $feed_data);
+        }
+        return $feed_data;
+    }
+}
+?>
