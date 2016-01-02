d5ca05fe42663c4c897657ff9f907966b1f96f4b
André Glüpker <git@wgmd.de>
2016-01-02 14:10:09 +0100

    New hack for new sinn-frei.com feed error
    Old fix (HTML umlauts in title) no longer needed
    Now an & in title fails the tt-rss parser

--- ff_sinnfrei/init.php
+++ ff_sinnfrei/init.php
@@ -4,8 +4,8 @@ class ff_sinnfrei extends Plugin {
     private $host;
 
     function about() {
-        return array(1.0,
-            "Fix feed of sinn-frei.com (remove html umlauts in title)",
+        return array(1.1,
+            "Fix feed of sinn-frei.com",
             "Joschasa");
     }
 
@@ -24,22 +24,24 @@ class ff_sinnfrei extends Plugin {
             _debug("New plugin up and running!");
 
             $suchmuster = array();
-            $suchmuster[0] = '/&uuml;/';
-            $suchmuster[1] = '/&auml;/';
-            $suchmuster[2] = '/&ouml;/';
-            $suchmuster[3] = '/&Uuml;/';
-            $suchmuster[4] = '/&Auml;/';
-            $suchmuster[5] = '/&Ouml;/';
-            $suchmuster[6] = '/&szlig/';
+            /* $suchmuster[0] = '/&uuml;/'; */
+            /* $suchmuster[1] = '/&auml;/'; */
+            /* $suchmuster[2] = '/&ouml;/'; */
+            /* $suchmuster[3] = '/&Uuml;/'; */
+            /* $suchmuster[4] = '/&Auml;/'; */
+            /* $suchmuster[5] = '/&Ouml;/'; */
+            /* $suchmuster[6] = '/&szlig;/'; */
+            $suchmuster[0] = '/&/';
 
             $ersetzungen = array();
-            $ersetzungen[0] = 'ü';
-            $ersetzungen[1] = 'ä';
-            $ersetzungen[2] = 'ö';
-            $ersetzungen[3] = 'Ü';
-            $ersetzungen[4] = 'Ä';
-            $ersetzungen[5] = 'Ö';
-            $ersetzungen[6] = 'ß';
+            /* $ersetzungen[0] = 'ü'; */
+            /* $ersetzungen[1] = 'ä'; */
+            /* $ersetzungen[2] = 'ö'; */
+            /* $ersetzungen[3] = 'Ü'; */
+            /* $ersetzungen[4] = 'Ä'; */
+            /* $ersetzungen[5] = 'Ö'; */
+            /* $ersetzungen[6] = 'ß'; */
+            $ersetzungen[0] = '&#038;';
 
             $feed_data = preg_replace($suchmuster, $ersetzungen, $feed_data);
         }
