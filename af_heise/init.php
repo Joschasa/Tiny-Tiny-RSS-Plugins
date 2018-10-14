2598c12c35a626fe7f63e9750df0a14ffeaeb7dc
André Glüpker <git@wgmd.de>
2018-10-14 15:54:14 +0200

    Unify af_heise, add techstage.de domain

--- af_heise/init.php
+++ af_heise/init.php
@@ -1,10 +1,11 @@
 <?php
+
 class Af_Heise extends Plugin {
 
     private $host;
 
     function about() {
-        return array(1.5,
+        return array(1.6,
             "Fetch content of heise.de feed",
             "Joschasa");
     }
@@ -19,8 +20,19 @@ class Af_Heise extends Plugin {
         $host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
     }
 
+    private function removeStuff($xpath, $filter) {
+        /* _debug("[RemoveStuff] Running filter " . $filter); */
+        $stuff = $xpath->query($filter);
+        foreach ($stuff as $removethis) {
+            /* _debug("[RemoveStuff] Removing tag &lt;" . $removethis->tagName . "&gt;"); */
+            /* _debug(htmlspecialchars($removethis->C14N())); */
+            $removethis->parentNode->removeChild($removethis);
+        }
+    }
+
     function hook_article_filter($article) {
-        if (strpos($article["link"], "heise.de") !== FALSE) {
+        if ((strpos($article["link"], "heise.de") !== FALSE)
+          || (strpos($article["link"], "techstage.de") !== FALSE)) {
             $link_orig = $article["link"]; //e.g.: "https://www.heise.de/newsticker/meldung/Waehrung-oder-Spekulationsobjekt-das-Bitcoin-Dilemma-Zahlen-oder-Zocken-3926657.html?wt_mc=rss.ho.beitrag.atom";
             $link_complete_article = substr($link_orig, 0, strrpos($link_orig, '?'));
 
@@ -35,13 +47,13 @@ class Af_Heise extends Plugin {
             $doc = new DOMDocument();
             @$doc->loadHTML(mb_convert_encoding(fetch_file_contents($link), 'HTML-ENTITIES', "UTF-8"));
 
-            $basenode = false;
+            $new_content = "";
 
             if ($doc) {
                 $xpath = new DOMXPath($doc);
 
                 //Remove unneeded stuff
-                $stuff = $xpath->query('(//script)|(//noscript)|(//a[@class="hinweis_anzeige"])|(//div[@class="shariff"])|(//p[@class="themenseiten"])|(//p[@class="permalink"])|(//p[@class="printversion"])|(//footer)|(//div[@class="adbottom"])|(//div[@class="rte__dossier"])|(//div[@class="publish-info"])|(//h2[@class="article__heading"])|(//p[@class="article-content__lead"])|(//div[@class="creator-info"])|(//div[@class="article-footer__content"])|(//section)');
+                $this->removeStuff($xpath, '(//script)|(//noscript)|(//a[@class="hinweis_anzeige"])|(//div[@class="shariff"])|(//p[@class="themenseiten"])|(//p[@class="permalink"])|(//p[@class="printversion"])|(//footer)|(//div[@class="adbottom"])|(//div[@class="rte__dossier"])|(//div[@class="publish-info"])|(//h2[@class="article__heading"])|(//p[@class="article-content__lead"])|(//div[@class="creator-info"])|(//div[@class="article-footer__content"])|(//section)');
                 foreach ($stuff as $removethis) {
                     $removethis->parentNode->removeChild($removethis);
                 }
@@ -56,14 +68,14 @@ class Af_Heise extends Plugin {
                 }
 
                 foreach ($entries as $entry) {
-                    $basenode = $entry;
+                    $new_content = $doc->saveHTML($entry);
                     break;
                 }
 
-                if ($basenode) {
-                    $article["content"] = $doc->saveHTML($basenode);
-                }
-                else {
+                if ($new_content) {
+                    /* _debug(htmlspecialchars($new_content)); */
+                    $article["content"] = $new_content;
+                } else {
                    //Problem here. Better output some infos into the feed to debug later
                    ob_start();
                    var_dump($stuff);
