12ea05db2df81f416f19bfcd0e3568a330b49339
André Glüpker <git@wgmd.de>
2018-10-14 18:02:09 +0200

    Improve af_rponline feed

--- af_rponline/init.php
+++ af_rponline/init.php
@@ -37,18 +37,21 @@ class Af_rponline extends Plugin {
                 $xpath = new DOMXPath($doc);
 
                 $this->removeStuff($xpath, '(//script)|(//noscript)|(//style)');
+                $this->removeStuff($xpath, '(//aside)|(//header)');
 
                 // Fetch Article Headline, Top Image, Article Paragraphs
-                $entries = $xpath->query('(//p[contains(@class, "park-article__intro ")])|(//p[@class="text"])');
+                $content = "";
+                $entries = $xpath->query('(//p[contains(@class, "park-article__intro")])|(//img[contains(@class, "park-image")])|(//p[contains(@class, "text")])');
                 foreach ($entries as $entry) {
                     $new_content = $doc->saveHTML($entry);
-                    break;
+                    /* _debug("Added: " . htmlspecialchars($new_content)); */
+                    $content = $content . $new_content;
                 }
 
-                if($new_content) {
-                    $new_content = preg_replace('/\s\s+/', ' ', $new_content);
-                    $article["content"] = $new_content;
-                    /* _debug(htmlspecialchars($new_content)); */
+                if($content) {
+                    $content = preg_replace('/\s\s+/', ' ', $content);
+                    $article["content"] = $content;
+                    /* _debug(htmlspecialchars($content)); */
                 }
             }
         }
