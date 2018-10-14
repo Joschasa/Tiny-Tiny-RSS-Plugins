be86ebe9d804ed19c8f76fdf7ba55901fa84d143
André Glüpker <git@wgmd.de>
2018-10-14 15:53:44 +0200

    Improve af_faz feed cleanup, unify source

--- af_faz/init.php
+++ af_faz/init.php
@@ -4,7 +4,7 @@ class Af_faz extends Plugin {
     private $host;
 
     function about() {
-        return array(1.2,
+        return array(1.3,
             "Fetch content of FAZ feed",
             "Joschasa");
     }
@@ -18,30 +18,40 @@ class Af_faz extends Plugin {
         $host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
     }
 
+    private function removeStuff($xpath, $filter) {
+        /* _debug("[RemoveStuff] Running filter " . $filter); */
+        $stuff = $xpath->query($filter);
+        foreach ($stuff as $removethis) {
+            /* _debug("[RemoveStuff] Removing tag &lt;" . $removethis->tagName . "&gt;"); */
+            /* _debug(htmlspecialchars($removethis->C14N())); */
+            $removethis->parentNode->removeChild($removethis);
+        }
+    }
+
     function hook_article_filter($article) {
         if (strpos($article["link"], "faz.net") !== FALSE) {
             $doc = new DOMDocument();
             @$doc->loadHTML(mb_convert_encoding(fetch_file_contents($article["link"]), 'HTML-ENTITIES', "UTF-8"));
 
-            $basenode = false;
+            $new_content = '';
 
             if ($doc) {
                 $xpath = new DOMXPath($doc);
 
-                $stuff = $xpath->query('(//script)|(//noscript)|(//div[@class="atc-ContainerSocialMedia"])|(//figure)|(//aside)');
-                foreach ($stuff as $removethis) {
-                    $removethis->parentNode->removeChild($removethis);
-                }
+                $this->removeStuff($xpath, '(//script)|(//noscript)|(//style)|(//figure)|(//aside)');
+                $this->removeStuff($xpath, '(//div[@class="ContainerSocialMedia"])|(//span[contains(@class, "Hidden")])|(//ul[contains(@class, "Author_Profile")])|(//div[contains(@class, "PlaceholderBox")])');
 
-                $entries = $xpath->query('(//div[@itemprop="articleBody"])|(//div[@class="single-entry-content"])');
+                $entries = $xpath->query('(//div[contains(@class, "atc-Text")])|(//div[@itemprop="articleBody"])|(//div[@class="single-entry-content"])');
 
-                $basenode = false;
                 foreach ($entries as $entry) {
-                    $basenode = $doc->saveHTML($entry);
+                    $new_content = $doc->saveHTML($entry);
+                    break;
                 }
 
-                if($basenode) {
-                    $article["content"] = $basenode;
+                if($new_content) {
+                    $new_content = preg_replace('/\s\s+/', ' ', $new_content);
+                    $article["content"] = $new_content;
+                    /* _debug(htmlspecialchars($new_content)); */
                 }
             }
         }
