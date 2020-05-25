7001a6aa726718e418c77f830590edce591d785e
André Glüpker <git@wgmd.de>
2020-05-25 18:47:52 +0200

    Rewrite reddit links to use old design

---
+++ af_old_reddit/init.php
@@ -0,0 +1,39 @@
+<?php
+class Af_Old_Reddit extends Plugin {
+
+    private $host;
+
+    function about() {
+        return array(1.0,
+            "Point towards old reddit",
+            "Joschasa");
+    }
+
+    function api_version() {
+        return 2;
+    }
+
+    function init($host) {
+        $this->host = $host;
+
+        $host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
+    }
+
+    private function removeStuff($xpath, $filter) {
+        /* _debug("[RemoveStuff] Running filter " . $filter); */
+        $stuff = $xpath->query($filter);
+        foreach ($stuff as $removethis) {
+            /* _debug("[RemoveStuff] Removing tag &lt;" . $removethis->tagName . "&gt;"); */
+            /* _debug(htmlspecialchars($removethis->C14N())); */
+            $removethis->parentNode->removeChild($removethis);
+        }
+    }
+
+    function hook_article_filter($article) {
+        if (strpos($article["link"], "www.reddit") !== FALSE) {
+            $article["link"] = str_replace("www.reddit", "old.reddit", $article["link"]);
+        }
+        return $article;
+    }
+}
+?>
